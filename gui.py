b028d9f6b325ee7d184769473f85d67dea31277e
William Hoffmann <whoffpen@gmail.com>
2013-04-05 20:36:14 -0400

    added answer validation in GUI version

--- gui.py
+++ gui.py
@@ -25,31 +25,36 @@ class GameDisplay(Frame):
         # Create a input box widget
         self.input_box = Entry(self, textvariable= self.user_input)
 
-        # Create a text box widget
+        # Create button widget
+        self.submit = Button(self, text="Answer", command = self.onClick)
+
+        # Create a text box widget to display equations
         self.tb = Text(self, width = 40, height = 5)
         
         # Bind a key to the widget
         self.input_box.bind('<Key-Return>', self.onClick)
 
+        # Add the buttons to the window
         self.tb.pack() 
         self.input_box.pack()
+        self.submit.pack()
     
         # Start by showing equations
         while(not self.game_logic.rb.isFull()):
-            self.tb.delete(1.0, 3.0)
+            self.tb.delete(1.0, END)
             self.tb.insert(1.0, self.game_logic.nextEquation())
             sleep(1)
 
     def onClick(self, event=None):
-        ''' Event that occurs when the answer button is pressed the incoming
-        event with either be from a click or the enter button being pressed
+        ''' Event that occurs when the answer button is pressed. The incoming
+        event will either be from a click, or the submit button being pressed
         '''
-        self.tb.delete(1.0, 3.0)
+        self.tb.delete('1.0', '3.0')
         # Verify the answer in the text box
         print self.user_input.get()
-        if(self.game_logic.validateAnswer(self.user_input.get())):
-            self.tb.insert(2.0, 'Correct!')
-        self.tb.insert(1.0, self.game_logic.nextEquation())
+        if(self.game_logic.validateAnswer(int(self.user_input.get()))):
+            self.tb.insert('2.0', 'Correct!')
+        self.tb.insert('1.0', self.game_logic.nextEquation())
 
         # Clear the input box
         self.user_input.set('')
